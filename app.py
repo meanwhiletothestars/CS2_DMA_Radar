32a9b417fcf6e40d672854accf3ecb5ca0cfa4e6
meanwhiletothestars <132231917+meanwhiletothestars@users.noreply.github.com>
2024-03-28 01:45:55 +0300

    Update app.py

--- app.py
+++ app.py
@@ -396,10 +396,12 @@ async def websocket_endpoint(websocket: WebSocket):
                                 "Weapon": str(weapon)
                             }
                             rawjsondata["Players"][str(i)] = player_data
+                            print(rawjsondata)
+                            print(player_data)
                     #except:
                         #continue
                 await websocket.send_text(rawjsondata)
-                print('data sended')        
+                print('data sended')
             #except Exception as e:
                 #print(e)
 
